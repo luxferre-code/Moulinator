254d614f42559ddecc322b0cdf8be131c2c3a450
adham <adham.berrakane.etu@univ-lille.fr>
2023-09-05 09:02:08 +0200

    Code affichage logo

--- src/fr/ulille/moulinator/Starting.java
+++ src/fr/ulille/moulinator/Starting.java
@@ -1,12 +1,29 @@
+package fr.ulille.moulinator;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Starting {
 
 
     public static void main(String[] args) {
-        System.out.println(" ");   
-                
+        File file = new File("Logo.txt");
+
+        try (FileReader fr = new FileReader(file))
+        {
+            int content;
+            while ((content = fr.read()) != -1) {
+                System.out.print((char) content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("");   
     }
+
 
 
 
