20b9a265b72c01125d72dc6678a723704648c45f
Ezechiel Leibovici <ezechiel.leibovici.etu@univ-lille.fr>
2023-09-05 10:42:15 +0200

    adding Game.java base

---
+++ src/main/fr/ulille/moulinator/CustomPlayer.java
@@ -0,0 +1,5 @@
+package fr.ulille.moulinator;
+
+public class CustomPlayer{
+     
+}
---
+++ src/main/fr/ulille/moulinator/Game.java
@@ -0,0 +1,7 @@
+package fr.ulille.moulinator;
+
+public final class Game {
+    
+    public static Joueur p1, p2;
+
+}
