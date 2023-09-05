b31368b66d21344be6f5e89fef046391c12ea870
Hocine CHEBOUT <hocine.chebout.etu@univ-lille.fr>
2023-09-05 11:31:26 +0200

    Fix test

--- src/test/fr/ulille/moulinator/TestJoueur.java
+++ src/test/fr/ulille/moulinator/TestJoueur.java
@@ -1,20 +1,29 @@
 package test.fr.ulille.moulinator;
 
 import fr.ulille.moulinator.Joueur;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestJoueur {
+    Joueur h;
+    Joueur v;
+    Joueur a;
 
-    public static void main(String[] args) {
-        Joueur h = new Joueur("Hocine");
-        Joueur v = new Joueur("Valentin");
-        Joueur a = new Joueur("Adham");
-        assert h.NAME.equals("Hocine");
-        System.out.println("success 1/3");
-        assert v.NAME.equals("Valentin");
-        System.out.println("success 2/3");
-        assert a.NAME.equals("Adham");
-        System.out.println("success 3/3");
-        System.out.println("All tests passed successfully.");
+    @BeforeEach
+    public void createJoueur() {
+        h = new Joueur("Hocine");
+        v = new Joueur("Valentin");
+        a = new Joueur("Adham");
+    }
+
+    @Test
+    public void testGetName(){
+        assertEquals("Hocine",h.getName());
+        assertEquals("Valentin",v.getName());
+        assertEquals("Adham",a.getName());
+        assertNotEquals("Hocine",v.getName());
     }
 
 
