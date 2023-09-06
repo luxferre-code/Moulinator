62dcd1a0320f16147a737241de50f109438c7922
Abdallah Toumji <abdallah.toumji.etu@univ-lille.fr>
2023-09-06 16:43:43 +0200

    Replace OneVSOne.java

--- src/main/fr/ulille/moulinator/gamemode/OneVSOne.java
+++ src/main/fr/ulille/moulinator/gamemode/OneVSOne.java
@@ -2,41 +2,34 @@ package fr.ulille.moulinator.gamemode;
 
 import java.util.Scanner;
 
+import fr.ulille.moulinator.Color;
 import fr.ulille.moulinator.CustomPlayer;
 import fr.ulille.moulinator.Game;
 import fr.ulille.moulinator.Joueur;
-/**
- * <p>La classe qui initialise le mode de jeu J1 VS J2</p>
- * @author HOCINE CHEBOUT
- * @author VALENTIN THUILLER
- * @author LEIBOVICI EZECHIEL
- * @author BARBEAU SIMON
- * @author TOUMJI ABDALLAH
- * @author BERRAKANE ADHAM
- */
+
 public class OneVSOne implements GameMode{
 
     @Override
     public void run(boolean isSave) {
         if(!isSave) {
-            System.out.println("Joueur 1 :");
+            System.out.println("Player 1 :");
             Game.p1 = CustomPlayer.makePlayer();
-            System.out.println("Joueur 2 :");
+            System.out.println("Player 2 :");
             Game.p2 = CustomPlayer.makePlayer();
         }
         Scanner sc = Game.SCANNER;
         boolean valide = false;
         while (!valide){
-            System.out.println("Avez vous bien créer vos joueurs oui/non : "  + Game.p1.toStringName() + "  " + Game.p2.toStringName());
+            System.out.println("Have you created your desired players yes/no : "  + Game.p1.toStringName() + "  " + Game.p2.toStringName());
             String choix = sc.next();
-            if (choix.toLowerCase().contains("non")){
+            if (choix.toLowerCase().contains("no")){
                 Game.p1.getColor().setUsed(false);
                 Game.p2.getColor().setUsed(false);
                 run();
-            } else if (choix.toLowerCase().contains("oui")){
+            } else if (choix.toLowerCase().contains("yes")){
                 valide = true;
             } else {
-                System.out.println("Choix invalide");
+                System.out.println("Invalid choice");
             }
 
         }
